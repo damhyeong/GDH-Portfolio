a7c371e489d5210c94a01d7e902265c4ffc735a3
damhyeong <rhdwhdals8@naver.com>
2024-01-04 23:06:45 +0900

    Creating Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Main from "./component/Main/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={"header"}>
+        <div className={"logo"}>logo</div>
+        <div className={"navigation"}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
       </header>
+      <Main/>
     </div>
   );
 }
