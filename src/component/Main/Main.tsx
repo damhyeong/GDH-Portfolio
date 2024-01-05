63841fd458a1a021490d0f4b8c7fbd58141dce14
damhyeong <rhdwhdals8@naver.com>
2024-01-06 02:58:13 +0900

    Change Some Component in Main Branch

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import "./Main.scss"
 
@@ -19,4 +20,51 @@ const Main = () => {
         </div>
     )
 }
+export default Main;*/
+
+import React, { useState, useEffect, useRef } from "react";
+import "./Main.scss";
+
+const Main: React.FC = () => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const helloWorldRef = useRef<HTMLDivElement | null>(null);
+
+    const callbackFunction = (entries: IntersectionObserverEntry[]) => {
+        const [entry] = entries;
+        setIsVisible(entry.isIntersecting);
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(callbackFunction, {
+            root: null,
+            threshold: 0.8 // 10%가 보일 때 애니메이션을 트리거합니다.
+        });
+
+        if (helloWorldRef.current) observer.observe(helloWorldRef.current);
+
+        return () => {
+            if (helloWorldRef.current) observer.unobserve(helloWorldRef.current);
+        };
+    }, []);
+
+    return (
+        <div className="main-component">
+            <div
+                className={`hello-world ${isVisible ? "fade-in" : "fade-out"}`}
+                ref={helloWorldRef}
+            >
+                Hello, World!
+            </div>
+            <div className={`hello-world-korean ${isVisible ? "fade-in" : "fade-out"}`}>
+                <div className={`korean-text-1 ${isVisible ? "fade-in" : "fade-out"}`}>
+                    다양한 세상과 코드의 만남.
+                </div>
+                <div className={`korean-text-2 ${isVisible ? "fade-in" : "fade-out"}`}>
+                    코드로 세상을 재해석합니다.
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export default Main;
