662e2dae0eccf9309584766abee32f2a9f1beeaa
damhyeong <rhdwhdals8@naver.com>
2024-01-10 00:07:15 +0900

    Complete Changing Color in Css Transition, and Saving Before Leaving Cafe

--- src/component/Project/Project.tsx
+++ src/component/Project/Project.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import './Project.scss'
 
 const Project = () => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+    const elementRef = useRef<HTMLDivElement>(null);
+
+    const handleScroll = useCallback(() => {
+        if(elementRef.current){
+            const position = elementRef.current.getBoundingClientRect();
+
+            if(position.top <= 400){
+                setIsActive(true);
+            } else {
+                setIsActive(false);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
+
     return (
-        <div className={"project-component"}>
+        <div ref={elementRef} className={`project-component ${isActive ? "active" : ""}`}>
             <div>
                 asdf
             </div>
