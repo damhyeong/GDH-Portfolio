2bf11efef507e37ea83ffb748e4d2fe0682a8463
damhyeong <rhdwhdals8@naver.com>
2024-01-09 20:31:13 +0900

    Commit Before Creating Project Component

---
+++ src/component/Project/Project.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Project.scss'
+
+const Project = () => {
+    return (
+        <div className={"project-component"}>
+            <div>
+                asdf
+            </div>
+        </div>
+    )
+}
+export default Project
